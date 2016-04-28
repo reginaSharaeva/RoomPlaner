22cc94d798da3e2ed5ec32eb7614a32775963e28
Регина Шараева <r.sharaeva3496@gmail.com>
2016-04-28 18:14:28 +0300

    Room

---
+++ src/Libraries/mouseCoords.js
@@ -0,0 +1,23 @@
+/**
+ * Объект работает с координатами курсора
+ */
+var MouseCoords = {
+    // X-координата
+    getX: function (e) {
+        if (e.pageX) {
+            return e.pageX;
+        } else if (e.clientX) {
+            return e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft;
+        }
+        return 0;
+    },
+    // Y-координата
+    getY: function (e) {
+        if (e.pageY) {
+            return e.pageY;
+        } else if (e.clientY) {
+            return e.clientY + (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientTop;
+        }
+        return 0;
+    }
+};
